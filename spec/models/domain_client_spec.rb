5f46b1469d1ce276f37356558c0a8ddbb7b58804
canisterism <ohtthstc@gmail.com>
2022-03-02 14:44:34 +0900

    :white_check_mark: add spec

--- spec/models/domain_client_spec.rb
+++ spec/models/domain_client_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DomainClient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'request_status' do
+    it 'returns the status of the request' do
+      domain = 'example.com'
+      client = DomainClient.new(domain)
+      expect(client.request_status).to eq(200)
+    end
+  end
 end
